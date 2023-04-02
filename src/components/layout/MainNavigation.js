38b482d2554474e19fe75de87be3e82e913a6a21
Manav Naharwal <manavnaharwalp@gmail.com>
2023-04-02 18:09:57 +0530

    Add sass/scss in the app

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from "./MainNavigation.module.scss";
 
 const MainNavigation = () => {
-  return <div>MainNavigation</div>;
+  return <div className={classes.container}>MainNavigation</div>;
 };
 
 export default MainNavigation;
