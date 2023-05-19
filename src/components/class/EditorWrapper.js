301f5a1fda22affe8cacc47e06d366468509f873
Ashish-ash <ak9810125073@gmail.com>
2023-05-19 11:17:19 +0530

    Added Announcement Container and ScrollToTop

--- src/components/class/EditorWrapper.js
+++ src/components/class/EditorWrapper.js
@@ -5,52 +5,52 @@ import classes from "./Editor.module.scss";
 import LoadingSpinner from "../progress/LoadingSpinner";
 
 const Editor = dynamic(() => import("./Editor"), {
-  ssr: false,
-  loading: () => <LoadingSpinner className={classes.spinner} />,
+	ssr: false,
+	loading: () => <LoadingSpinner className={classes.spinner} />,
 });
 
 const EditorWrapper = (props) => {
-  const { teacher } = props;
-  const [textEditor, setTextEditor] = useState(false);
-  const [content, setContent] = useState("");
+	const { teacher } = props;
+	const [textEditor, setTextEditor] = useState(false);
+	const [content, setContent] = useState("");
 
-  const cancelButton = () => {
-    setContent("");
-    setTextEditor(false);
-  };
+	const cancelButton = () => {
+		setContent("");
+		setTextEditor(false);
+	};
 
-  const getValue = (value) => {
-    setContent(value);
-  };
+	const getValue = (value) => {
+		setContent(value);
+	};
 
-  return (
-    <>
-      {!textEditor && (
-        <div className={classes["class__announce"]}>
-          <Image
-            width={60}
-            height={60}
-            src={teacher.credentials.userImage}
-            alt="User image"
-          />
-          <input
-            type="text"
-            placeholder="Announce something to your class"
-            onClick={() => setTextEditor(true)}
-          />
-        </div>
-      )}
-      {textEditor && (
-        <div className={classes.editor}>
-          <Editor contents={content} getValue={getValue} />
-          <div className={classes.container}>
-            <button onClick={props.createAnnouncement}>Post</button>
-            <button onClick={cancelButton}>Cancel</button>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{!textEditor && (
+				<div className={classes["class__announce"]}>
+					<Image
+						width={60}
+						height={60}
+						src={teacher.credentials.userImage}
+						alt="User image"
+					/>
+					<input
+						type="text"
+						placeholder="Announce something to your class"
+						onClick={() => setTextEditor(true)}
+					/>
+				</div>
+			)}
+			{textEditor && (
+				<div className={classes.editor}>
+					<Editor contents={content} getValue={getValue} />
+					<div className={classes.container}>
+						<button onClick={props.createAnnouncement}>Post</button>
+						<button onClick={cancelButton}>Cancel</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default React.memo(EditorWrapper);
