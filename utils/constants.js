e94b42d2299e93c720ff92d4902bc899e5d40509
Manav Naharwal <manavnaharwalp@gmail.com>
2023-05-31 21:38:43 +0530

    feat: add pin-announcement feat

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,7 @@
 const constants = {
   SUCCESS_TOAST: "successToast",
+  SUCCESS_DELETE_TOAST: "successDeleteToast",
+  SUCCESS_CREATE_EDIT_TOAST: "successCreateEditToast",
   ERROR_TOAST: "errorToast",
   SERVER_ERROR_TOAST: "serverErrorToast",
 };
