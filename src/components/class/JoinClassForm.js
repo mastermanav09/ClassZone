54d59dd88db12811bc230d7f97e01341885fa302
Ashish-ash <ak9810125073@gmail.com>
2023-06-04 20:36:14 +0530

    Created people page and added search functionality

--- src/components/class/JoinClassForm.js
+++ src/components/class/JoinClassForm.js
@@ -6,82 +6,83 @@ import LoadingSpinner from "../progress/LoadingSpinner";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { joinClass } from "../../../utils/store/reducers/class";
+import "react-responsive-modal/styles.css";
 const JoinClassForm = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const { toggleJoinClassModal, showJoinClassModal } = props;
-  const form = useForm();
-  const { register, handleSubmit } = form;
-  const router = useRouter();
-  const dispatch = useDispatch();
+	const [isLoading, setIsLoading] = useState(false);
+	const { toggleJoinClassModal, showJoinClassModal } = props;
+	const form = useForm();
+	const { register, handleSubmit } = form;
+	const router = useRouter();
+	const dispatch = useDispatch();
 
-  const onSubmit = ({ classCode }) => {
-    dispatch(
-      joinClass({
-        toggleJoinClassModal,
-        setIsLoading,
-        router,
-        classId: classCode,
-      })
-    );
-  };
+	const onSubmit = ({ classCode }) => {
+		dispatch(
+			joinClass({
+				toggleJoinClassModal,
+				setIsLoading,
+				router,
+				classId: classCode,
+			})
+		);
+	};
 
-  return (
-    <div>
-      <Modal
-        classNames={classes["react-responsive-modal-modal"]}
-        onClose={toggleJoinClassModal}
-        center
-        open={showJoinClassModal}
-        animationDuration={200}
-        styles={{
-          overlay: {
-            position: "fixed",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.75)",
-          },
-          closeIcon: {
-            fill: "#ff0000",
-          },
-          modal: {
-            borderRadius: "10px",
-          },
-        }}
-      >
-        <div className={classes.modal}>
-          <h2>Join class</h2>
-          <form
-            className={classes.form}
-            id="joinClass-form"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div>
-              <label htmlFor="classCode">Class code</label>
-              <input
-                type="text"
-                required
-                placeholder="Class code"
-                id="classCode"
-                {...register("classCode")}
-              />
-            </div>
-          </form>
-          <div className={classes.btn}>
-            <button type="submit" form="joinClass-form" disabled={isLoading}>
-              {isLoading ? (
-                <LoadingSpinner className={classes.spinner} />
-              ) : (
-                "Join"
-              )}
-            </button>
-            <button onClick={toggleJoinClassModal}>Cancel</button>
-          </div>
-        </div>
-      </Modal>
-    </div>
-  );
+	return (
+		<div>
+			<Modal
+				classNames={classes["react-responsive-modal-modal"]}
+				onClose={toggleJoinClassModal}
+				center
+				open={showJoinClassModal}
+				animationDuration={200}
+				styles={{
+					overlay: {
+						position: "fixed",
+						top: 0,
+						left: 0,
+						right: 0,
+						bottom: 0,
+						backgroundColor: "rgba(0, 0, 0, 0.75)",
+					},
+					closeIcon: {
+						fill: "#ff0000",
+					},
+					modal: {
+						borderRadius: "10px",
+					},
+				}}
+			>
+				<div className={classes.modal}>
+					<h2>Join class</h2>
+					<form
+						className={classes.form}
+						id="joinClass-form"
+						onSubmit={handleSubmit(onSubmit)}
+					>
+						<div>
+							<label htmlFor="classCode">Class code</label>
+							<input
+								type="text"
+								required
+								placeholder="Class code"
+								id="classCode"
+								{...register("classCode")}
+							/>
+						</div>
+					</form>
+					<div className={classes.btn}>
+						<button type="submit" form="joinClass-form" disabled={isLoading}>
+							{isLoading ? (
+								<LoadingSpinner className={classes.spinner} />
+							) : (
+								"Join"
+							)}
+						</button>
+						<button onClick={toggleJoinClassModal}>Cancel</button>
+					</div>
+				</div>
+			</Modal>
+		</div>
+	);
 };
 
 export default JoinClassForm;
