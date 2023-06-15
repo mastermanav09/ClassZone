d29ecd6d2063a109d48b7595c2c88d563321ccdc
Manav Naharwal <manavnaharwalp@gmail.com>
2023-06-16 02:28:50 +0530

    refactor: refactor code

---
+++ src/pages/classes/[classId]/classwork.js
@@ -0,0 +1,8 @@
+import Classwork from "@/components/class/classwork/Classwork";
+import React from "react";
+
+const ClassworkPage = () => {
+  return <Classwork />;
+};
+
+export default ClassworkPage;
