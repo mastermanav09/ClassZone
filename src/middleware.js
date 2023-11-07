91278fa50cfa3cb68b09929d73978a21cb6688a1
Manav Naharwal <manavnaharwalp@gmail.com>
2023-11-08 00:30:34 +0530

    fix: fix login redirect 24

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,78 @@
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
+// import { pathToRegexp } from "path-to-regexp";
+// import * as jsonwebtoken from "jsonwebtoken";
+
+// export async function middleware(request) {
+//   // const session = await getToken({ req: request, raw: true });
+
+//   try {
+//     const jwtEncodedToken = request?.cookies?.get(
+//       "next-auth.session-token"
+//     ).value;
+//     const session = jsonwebtoken.verify(
+//       jwtEncodedToken ?? "",
+//       process.env.NEXTAUTH_SECRET
+//     );
+//     const PUBLIC_FILE = /\.(.*)$/;
+//     const authRegex = /^\/api\/auth\//;
+//     const { pathname } = request.nextUrl;
+//     console.log("token", session);
+//     if (
+//       pathname.startsWith("/_next") ||
+//       pathname.startsWith("/static") ||
+//       PUBLIC_FILE.test(pathname)
+//     ) {
+//       return NextResponse.next();
+//     }
+
+//     if (pathname === "/login" || pathname === "/register") {
+//       return NextResponse.next();
+//     }
+
+//     if (authRegex.test(request.nextUrl.pathname)) {
+//       return NextResponse.next();
+//     }
+
+//     if (session) {
+//       const requestHeaders = new Headers(request.headers);
+//       requestHeaders.set("x-user-id", session._id);
+
+//       return NextResponse.next({
+//         request: {
+//           headers: requestHeaders,
+//         },
+//       });
+//     } else {
+//       // if (pathname !== "/login") {
+//       // return NextResponse.redirect(new URL("/login", request.url));
+//       // }
+//     }
+//   } catch (error) {
+//     console.log("error", error);
+//     // return NextResponse.redirect(new URL("/login", request.url));
+//   }
+// }
+
+// // const matcher = pathToRegexp();
+// export const config = {
+//   // matcher: [
+//   //   "/((?!register|api/auth|login|unauthorized|_next/image|auth).{1,})",
+//   // ],
+//   // matcher: ["/((?!register|api|login|$).*)"],
+//   matcher: ["/((?!api/auth|login|register|_next).*)"],
+//   // matcher: ["/", "/unauthorized", "/classes/:path*", "/api/class", "/api/user"],
+// };
+
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-import { pathToRegexp } from "path-to-regexp";
 
 export async function middleware(request) {
-  const session = await getToken({ req: request, raw: true });
+  const session = await getToken({ req: request });
   const PUBLIC_FILE = /\.(.*)$/;
   const authRegex = /^\/api\/auth\//;
   const { pathname } = request.nextUrl;
-  console.log("token", session);
+  console.log("session", session);
   if (
     pathname.startsWith("/_next") ||
     pathname.startsWith("/static") ||
@@ -15,37 +80,29 @@ export async function middleware(request) {
   ) {
     return NextResponse.next();
   }
-
-  // if (pathname === "/login" || pathname === "/register") {
-  //   return NextResponse.next();
-  // }
-
-  // if (authRegex.test(request.nextUrl.pathname)) {
-  //   return NextResponse.next();
-  // }
-
+  if (pathname === "/login" || pathname === "/register") {
+    return NextResponse.next();
+  }
+  if (authRegex.test(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
   if (session) {
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set("x-user-id", session._id);
-
     return NextResponse.next({
       request: {
         headers: requestHeaders,
       },
     });
   } else {
-    // if (pathname !== "/login") {
-    // return NextResponse.redirect(new URL("/login", request.url));
-    // }
+    console.log(pathname);
+    if (pathname !== "/login") {
+      // return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
 }
 
-// const matcher = pathToRegexp();
 export const config = {
-  // matcher: [
-  //   "/((?!register|api/auth|login|unauthorized|_next/image|auth).{1,})",
-  // ],
-  // matcher: ["/((?!register|api|login|$).*)"],
-  matcher: ["/((?!api/auth).*)"],
-  // matcher: ["/", "/unauthorized", "/classes/:path*", "/api/class", "/api/user"],
+  // matcher: ["/((?!/api/auth|!_next).*)"],
+  matcher: ["/((?!_next).*)"],
 };
