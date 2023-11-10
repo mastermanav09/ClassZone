fad571a8f3d6a8a11d0b90626a017342431a647f
Manav Naharwal <manavnaharwalp@gmail.com>
2023-11-11 03:01:51 +0530

    fix: fix 3

--- src/middleware.js
+++ src/middleware.js
@@ -37,6 +37,6 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!api/auth|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/image|favicon.ico|login|register).*)"],
   // matcher: ["/((?!register|login|api/auth).{1,})"],
 };
